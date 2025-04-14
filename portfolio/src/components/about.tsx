bbf02c0145ed62320d7be7c6d07383b0973ee25e
Thibaud Guadagna <thibaud.guadagna@gmail.com>
2025-04-14 13:21:10 +0200

    responsive v2

--- portfolio/src/components/about.tsx
+++ portfolio/src/components/about.tsx
@@ -4,39 +4,47 @@ function About() {
     <section className="bg-[#E1EBED] m-5 -mt-5">
       <section>
     <div>
-      <h2 className="text-[#331c52] font-bold text-3xl m-5 p-5">About Me</h2>
+      <h2 className="text-[#331c52] font-bold text-3xl p-5">About Me</h2>
     </div>
   
-    <div className="-mt-5 flex justify-between">
-      <p className="text-[#331c52] p-5">
-        Après avoir passé plus de 3 ans dans le consulting et près de 5 ans dans le domaine des ressources humaines, j'ai décidé de me reconvertir dans le développement web, une passion qui m'anime depuis toujours. Mon objectif est de créer des applications web performantes et intuitives qui répondent aux besoins des utilisateurs.
-      </p>
-      <img src="/code.svg" alt="baliseHtml" className='h-50 m-5 opacity-20 sm:w-full' />
-    </div>
+    <div className="-mt-5 flex sm:items-center w-full">
+  <p className="text-[#331c52] p-5 w-full sm:w-1/2 sm:z-10">
+    Après avoir passé plus de 3 ans dans le consulting et près de 5 ans dans le domaine des ressources humaines, j'ai décidé de me reconvertir dans le développement web, une passion qui m'anime depuis toujours. Mon objectif est de créer des applications web performantes et intuitives qui répondent aux besoins des utilisateurs.
+  </p>
+
+  <div className="hidden sm:block sm:w-1/2">
+    <img src="/code.svg" alt="baliseHtml" className="w-full h-50 opacity-20" />
+  </div>
+</div>
     </section>
   
-    <div className="flex flex-col items-end m-5">
-      <h2 className="text-[#331c52] font-bold text-3xl">Mes compétences</h2>
-      <p className="text-[#331c52] font-bold text-xl">Stacks récemment utilisés :</p>
+    <div className="flex flex-col items-end w-full">
+      <h2 className="text-[#331c52] font-bold text-3xl mr-6">Mes compétences</h2>
+      <p className="text-[#331c52] font-bold text-xl mb-6 mr-5">Stacks récemment utilisés :</p>
     </div>
   
+    <section className="flex w-full items-center px-3 py-4">
+  {/* Colonne gauche avec le logo Merge */}
+  <div className="w-1/2 flex md:justify-center md:items-center md:ml-5">
+    <img src="/mergelogo.svg" alt="Merge Logo" className="h-32 opacity-20 max-w-full" />
+  </div>
+
+  {/* Colonne droite avec 2 lignes de 3 logos */}
+  <div className="w-1/2 grid grid-cols-3 gap-4 place-items-center">
+    <img src="/logojavascript.svg" alt="JavaScript Logo" className="bg-[#331c52] h-12 p-2 rounded-sm" />
+    <img src="/htmllogo.svg" alt="HTML Logo" className="bg-[#331c52] h-12 p-1 rounded-sm" />
+    <img src="/csslogo.svg" alt="CSS Logo" className="bg-[#331c52] h-12 p-1 rounded-sm" />
+    <img src="/react.svg" alt="React Logo" className="bg-[#331c52] h-12 w-12 p-1 rounded-sm" />
+    <img src="/jsonlogo.svg" alt="JSON Logo" className="bg-[#331c52] h-12 p-1 rounded-sm" />
+    <img src="/vscodelogo.svg" alt="VSCode Logo" className="bg-[#331c52] h-12 p-1 rounded-sm" />
+  </div>
+</section>
+
+
+
+
+
     
-    <div className="flex justify-between items-center m-5 ">
-      <img src="/mergelogo.svg" alt="Merge Logo" className='h-40 opacity-20 ml-20 mb-10 -mt-20' />
-      
-      <div className="flex flex-col items-end gap-2">
-        <div className="flex gap-3 px-5 py-1">
-          <img src="/logojavascript.svg" alt="JavaScript Logo" className='bg-[#331c52] h-12 p-2 rounded-sm' />
-          <img src="/htmllogo.svg" alt="HTML Logo" className='bg-[#331c52] h-12 p-1 rounded-sm' />
-          <img src="/csslogo.svg" alt="CSS Logo" className='bg-[#331c52] h-12 p-1 rounded-sm' />
-        </div>
-        <div className="flex gap-3 px-5 py-1">
-          <img src="/react.svg" alt="React Logo" className='bg-[#331c52] h-12 w-12 p-1 rounded-sm' />
-          <img src="/jsonlogo.svg" alt="JSON Logo" className='bg-[#331c52] h-12 p-1 rounded-sm' />
-          <img src="/vscodelogo.svg" alt="VSCode Logo" className='bg-[#331c52] h-12 p-1 rounded-sm' />
-        </div>
-      </div>
-    </div>
   </section>
   
   );
--- portfolio/src/components/header.tsx
+++ portfolio/src/components/header.tsx
@@ -10,25 +10,32 @@ function Header() {
       </div>
 
       {/* Navigation */}
-      <ul className="flex justify-between sm:text-white sm:m-5 bg-[#8121d0] rounded-t-md h-12 px-4 mx-5">
-        <li className="text-white py-2 md:py-3.5 sm:px-5">Accueil</li>
-        <li className="text-white py-2 md:py-3.5 sm:px-5">Projets</li>
-        <li className="text-white py-2 md:py-3.5 sm:px-5">Contact</li>
-      </ul>
+      <ul className="flex justify-between sm:text-white sm:m- bg-[#8121d0] rounded-t-md h-12 md:h-14 px-1 mx-5">
+  <li className="text-white py-2 md:py-1 md:px-5">Accueil</li>
+  <li className="text-white py-2 md:py-1 md:px-5">Projets</li>
+  <li className="text-white py-2 md:py-1 md:px-5">Contact</li>
+  <li className="py-2 md:py-1 md:px-5">
+    <button className='bg-white text-black font-bold rounded-md p-1 flex items-center text-sm'>
+      <img src='/enveloppe.svg' alt="Enveloppe icon" className="w-5 h-5 mr-2 " />
+      Recrutez moi !
+    </button>
+  </li>
+</ul>
+
 
       {/* Intro avec la vidéo à droite des titres */}
       <div className='bg-[#331c52] sm:m-5 p-5 md:-mt-5 flex flex-col sm:flex-row items-center sm:items-start mx-5 '>
         <div className='flex flex-col sm:mr-5 w-full'>
-          <h1 className='text-white text-2xl font-bold ml-5 mb-10 sm:mb-20'>Bonjour👋</h1>
-          <h2 className='text-white/15 text-3xl sm:text-4xl ml-5 mb-10 sm:mb-20'>
+          <h1 className='text-white text-2xl font-bold  mb-10 sm:mb-20'>Bonjour👋</h1>
+          <h2 className='text-white/15 text-3xl sm:text-4xl mb-10 sm:mb-20'>
             Je m'appelle Thibaud<br />Je suis développeur Web.
           </h2>
-          <p className='text-white text-xl sm:text-2xl ml-5 mb-5'>Je construis des sites sur le Web.</p>
+          <p className='text-white text-xl sm:text-2xl mb-5'>Je construis des sites sur le Web.</p>
 
           {/* Liens sociaux sous les titres */}
           <div className='flex mt-5 ml-5'>
             <a href="https://www.linkedin.com/in/thibaud-guadagna-893124186/" target='_blank' rel="noopener noreferrer">
-              <img src="/ri--linkedin-fill.svg" alt="linkedin" className='w-10 h-10 ml-5 bg-white p-2 rounded-full opacity-25' />
+              <img src="/ri--linkedin-fill.svg" alt="linkedin" className='w-10 h-10  bg-white p-2 rounded-full opacity-25' />
             </a>
             <a href="https://github.com/Thibaud-Guadagna" target='_blank' rel="noopener noreferrer">
               <img src="/mingcute--github-fill.svg" alt="GitHub" className='w-10 h-10 ml-5 bg-white p-2 rounded-full opacity-25' />
@@ -41,7 +48,7 @@ function Header() {
           autoPlay
           loop
           muted
-          src="/video.mp4"
+          src="/video2.mp4"
           className="w-64 sm:w-72 h-auto rounded-lg shadow-lg mt-5 sm:mt-0"
         />
       </div>
@@ -53,13 +60,13 @@ function Header() {
           <h3 className='text-white text-xl sm:text-2xl'>Projets développés pour le moment</h3>
         </div>
 
-        <div className='flex gap-3 p-1 flex-wrap justify-center sm:justify-start'>
+        <div className='flex p-1 flex-wrap justify-center sm:justify-start'>
           <img src="/logojavascript.svg" alt="JavaScript Logo" className='h-12 p-1' />
-          <img src="/htmllogo.svg" alt="HTML Logo" className='h-12 p-1' />
-          <img src="/csslogo.svg" alt="CSS Logo" className='h-12 p-1' />
-          <img src="/react.svg" alt="React Logo" className='h-12 p-1' />
-          <img src="/jsonlogo.svg" alt="JSON Logo" className='h-12 p-1' />
-          <img src="/vscodelogo.svg" alt="VSCode Logo" className='h-12 p-1' />
+          <img src="/htmllogo.svg" alt="HTML Logo" className='h-11' />
+          <img src="/csslogo.svg" alt="CSS Logo" className='h-11 p-1' />
+          <img src="/react.svg" alt="React Logo" className='h-11 p-1' />
+          <img src="/jsonlogo.svg" alt="JSON Logo" className='h-11 p-1' />
+          <img src="/vscodelogo.svg" alt="VSCode Logo" className='h-11 p-1' />
         </div>
       </section>
     </header>
